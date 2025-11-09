5737a5be67e6b47c955009b68d3df2d60144dbb3
John Yk <yikok.yong@gmail.com>
2025-11-09 09:17:27 +0800

    feat: add native code

--- android/src/main/java/com/ykyong/reactnativecrypto/ReactNativeCryptoModule.kt
+++ android/src/main/java/com/ykyong/reactnativecrypto/ReactNativeCryptoModule.kt
@@ -1,7 +1,11 @@
 package com.ykyong.reactnativecrypto
 
+import android.util.Base64
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
+import java.security.MessageDigest
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 
 @ReactModule(name = ReactNativeCryptoModule.NAME)
 class ReactNativeCryptoModule(reactContext: ReactApplicationContext) :
@@ -11,10 +15,47 @@ class ReactNativeCryptoModule(reactContext: ReactApplicationContext) :
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  override fun multiply(a: Double, b: Double): Double {
-    return a * b
+  private fun hexToBytes(hex: String): ByteArray {
+    check(hex.length % 2 == 0) { "Must have an even length" }
+    return hex.chunked(2)
+        .map { it.toInt(16).toByte() }
+        .toByteArray()
+  }
+
+  override fun sha256(input: String): String {
+    val bytes = input.toByteArray(Charsets.UTF_8)
+    val digest = MessageDigest.getInstance("SHA-256")
+    val hashBytes = digest.digest(bytes)
+    return Base64.encodeToString(hashBytes, Base64.NO_WRAP)
+  }
+
+  override fun sha1(input: String): String {
+    val bytes = input.toByteArray(Charsets.UTF_8)
+    val digest = MessageDigest.getInstance("SHA-1")
+    val hashBytes = digest.digest(bytes)
+    return Base64.encodeToString(hashBytes, Base64.NO_WRAP)
+  }
+
+  override fun hmacSha256(key: String, data: String): String {
+    val sha256Hmac = Mac.getInstance("HmacSHA256")
+    val secretKey = SecretKeySpec(key.toByteArray(Charsets.UTF_8), "HmacSHA256")
+    sha256Hmac.init(secretKey)
+    val hashBytes = sha256Hmac.doFinal(data.toByteArray(Charsets.UTF_8))
+    return Base64.encodeToString(hashBytes, Base64.NO_WRAP)
+  }
+
+  override fun convertHashEncoding(hash: String, from: String, to: String): String {
+    val bytes = when (from) {
+      "hex" -> hexToBytes(hash)
+      "base64" -> Base64.decode(hash, Base64.DEFAULT)
+      else -> throw IllegalArgumentException("Invalid 'from' encoding. Must be 'hex' or 'base64'.")
+    }
+
+    return when (to) {
+      "hex" -> bytes.joinToString("") { "%02x".format(it) }
+      "base64" -> Base64.encodeToString(bytes, Base64.NO_WRAP)
+      else -> throw IllegalArgumentException("Invalid 'to' encoding. Must be 'hex' or 'base64'.")
+    }
   }
 
   companion object {
---
+++ specs/ReactNativeCrypto.ts
@@ -0,0 +1,15 @@
+import type { TurboModule } from 'react-native';
+import { TurboModuleRegistry } from 'react-native';
+
+export interface Spec extends TurboModule {
+  hmacSha256(key: string, message: string): Promise<string>;
+  sha256(message: string): Promise<string>;
+  sha1(message: string): Promise<string>;
+  convertHashEncoding(
+    hash: string,
+    fromEncoding: 'hex' | 'base64',
+    toEncoding: 'hex' | 'base64'
+  ): Promise<string>;
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>('ReactNativeCrypto');
