16877215fe58046b5fae0958b3b113fac756d960
John Yk <yikok.yong@gmail.com>
2025-11-09 11:21:28 +0800

    feat: update cryptographic

--- android/src/main/java/com/ykyong/reactnativecrypto/ReactNativeCryptoModule.kt
+++ android/src/main/java/com/ykyong/reactnativecrypto/ReactNativeCryptoModule.kt
@@ -1,6 +1,7 @@
 package com.ykyong.reactnativecrypto
 
 import android.util.Base64
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
 import java.security.MessageDigest
@@ -22,39 +23,60 @@ class ReactNativeCryptoModule(reactContext: ReactApplicationContext) :
         .toByteArray()
   }
 
-  override fun sha256(input: String): String {
-    val bytes = input.toByteArray(Charsets.UTF_8)
-    val digest = MessageDigest.getInstance("SHA-256")
-    val hashBytes = digest.digest(bytes)
-    return Base64.encodeToString(hashBytes, Base64.NO_WRAP)
-  }
-
-  override fun sha1(input: String): String {
-    val bytes = input.toByteArray(Charsets.UTF_8)
-    val digest = MessageDigest.getInstance("SHA-1")
-    val hashBytes = digest.digest(bytes)
-    return Base64.encodeToString(hashBytes, Base64.NO_WRAP)
-  }
-
-  override fun hmacSha256(key: String, data: String): String {
-    val sha256Hmac = Mac.getInstance("HmacSHA256")
-    val secretKey = SecretKeySpec(key.toByteArray(Charsets.UTF_8), "HmacSHA256")
-    sha256Hmac.init(secretKey)
-    val hashBytes = sha256Hmac.doFinal(data.toByteArray(Charsets.UTF_8))
-    return Base64.encodeToString(hashBytes, Base64.NO_WRAP)
-  }
-
-  override fun convertHashEncoding(hash: String, from: String, to: String): String {
-    val bytes = when (from) {
-      "hex" -> hexToBytes(hash)
-      "base64" -> Base64.decode(hash, Base64.DEFAULT)
-      else -> throw IllegalArgumentException("Invalid 'from' encoding. Must be 'hex' or 'base64'.")
+  override fun sha256(message: String, promise: Promise): Unit {
+    try {
+      val bytes = message.toByteArray(Charsets.UTF_8)
+      val digest = MessageDigest.getInstance("SHA-256")
+      val hashBytes = digest.digest(bytes)
+      val result = Base64.encodeToString(hashBytes, Base64.NO_WRAP)
+      promise.resolve(result)
+    } catch (e: Exception) {
+      promise.reject(e)
     }
+  }
 
-    return when (to) {
-      "hex" -> bytes.joinToString("") { "%02x".format(it) }
-      "base64" -> Base64.encodeToString(bytes, Base64.NO_WRAP)
-      else -> throw IllegalArgumentException("Invalid 'to' encoding. Must be 'hex' or 'base64'.")
+  override fun sha1(message: String, promise: Promise): Unit {
+    try {
+      val bytes = message.toByteArray(Charsets.UTF_8)
+      val digest = MessageDigest.getInstance("SHA-1")
+      val hashBytes = digest.digest(bytes)
+      val result = Base64.encodeToString(hashBytes, Base64.NO_WRAP)
+      promise.resolve(result)
+    } catch (e: Exception) {
+      promise.reject(e)
+    }
+  }
+
+  override fun hmacSha256(key: String, message: String, promise: Promise): Unit {
+    try {
+      val sha256Hmac = Mac.getInstance("HmacSHA256")
+      val secretKey = SecretKeySpec(key.toByteArray(Charsets.UTF_8), "HmacSHA256")
+      sha256Hmac.init(secretKey)
+      val hashBytes = sha256Hmac.doFinal(message.toByteArray(Charsets.UTF_8))
+      val result = Base64.encodeToString(hashBytes, Base64.NO_WRAP)
+      promise.resolve(result)
+    } catch (e: Exception) {
+      promise.reject(e)
+    }
+  }
+
+  override fun convertHashEncoding(hash: String, fromEncoding: String, toEncoding: String, promise: Promise): Unit {
+    try {
+      val bytes = when (fromEncoding) {
+        "hex" -> hexToBytes(hash)
+        "base64" -> Base64.decode(hash, Base64.DEFAULT)
+        else -> throw IllegalArgumentException("Invalid 'from' encoding. Must be 'hex' or 'base64'.")
+      }
+
+      val result = when (toEncoding) {
+        "hex" -> bytes.joinToString("") { "%02x".format(it) }
+        "base64" -> Base64.encodeToString(bytes, Base64.NO_WRAP)
+        else -> throw IllegalArgumentException("Invalid 'to' encoding. Must be 'hex' or 'base64'.")
+      }
+
+      promise.resolve(result)
+    } catch (e: Exception) {
+      promise.reject(e)
     }
   }
 
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,3 +1,70 @@
+import { hmacSha256, sha1, sha256 } from '@yk-yong/react-native-crypto';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const helloWorldHashBase64 = '3/1gIbsr1bCvZ2KQgJ7DpTGR3YHH9wpLKGiKNiGCmG8=';
+const helloWorldSha1Base64 = 'CgqfKmdylCVXq1NV12r0Qvj2XgE=';
+const helloWorldHmacSha256Base64 =
+  'Fu5SX2yUT/SaNozVk+t7cog7FEVse1g7uk/5c/9LMPk=';
+
 export default function App() {
-  return null;
+  const [hash, setHash] = useState<string>('');
+  const [hashSha1, setHashSha1] = useState<string>('');
+  const [hmac, setHmac] = useState<string>('');
+
+  useEffect(() => {
+    async function computeHash() {
+      const result = await sha256('Hello, World!');
+      setHash(result);
+    }
+    computeHash();
+  }, []);
+
+  useEffect(() => {
+    async function computeHashSha1() {
+      const result = await sha1('Hello, World!');
+      setHashSha1(result);
+    }
+    computeHashSha1();
+  }, []);
+
+  useEffect(() => {
+    async function computeHmac() {
+      // Example HMAC computation (using sha256 as the hash function)
+      const key = 'secret-key';
+      const message = 'Hello, World!';
+      const hmacResult = await hmacSha256(key, message);
+      setHmac(hmacResult);
+    }
+    computeHmac();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>
+        {hash === helloWorldHashBase64
+          ? 'SHA256 is valid!'
+          : 'SHA256 is invalid.'}
+      </Text>
+      <Text>
+        {hashSha1 === helloWorldSha1Base64
+          ? ' SHA1 is valid!'
+          : ' SHA1 is invalid.'}
+      </Text>
+      <Text>
+        {hmac === helloWorldHmacSha256Base64
+          ? ' HMAC-SHA256 is valid!'
+          : ' HMAC-SHA256 is invalid.'}
+      </Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
